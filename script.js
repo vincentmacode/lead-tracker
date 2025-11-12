006b4017720d911417f65a8f98afec33e836898c
vincentmacode <vincentma.code@gmail.com>
2025-11-11 22:23:35 -0500

    Adding localStorage
    Added localStorage to save inputs into array.

--- script.js
+++ script.js
@@ -2,13 +2,24 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads"))
 
+//rendering from localStorage
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+//button click event listener
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    console.log( localStorage.getItem("myLeads"))
     inputEl.value = ""
     renderLeads()
 })
 
+//function behaviour
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
@@ -19,7 +30,6 @@ function renderLeads() {
                         </a>
                     </li>
                 `
-        console.log(listItems)
     }
     ulEl.innerHTML = listItems
 }
